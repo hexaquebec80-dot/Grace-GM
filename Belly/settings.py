92a6da4f14ced41bb2d217ada1b0086c22a4ecb3
Hexa Quebec <hexaquebec80@gmail.com>
2026-09-25 19:27:48 -0400

    base.html

--- Belly/settings.py
+++ Belly/settings.py
@@ -298,3 +298,33 @@ LOGGING = {
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "/cart/"
+
+
+
+
+# --------------------------------------------------
+# CLOUDINARY
+# --------------------------------------------------
+
+CLOUDINARY_CLOUD_NAME = config(
+    "CLOUDINARY_CLOUD_NAME",
+    default="",
+)
+
+CLOUDINARY_API_KEY = config(
+    "CLOUDINARY_API_KEY",
+    default="",
+)
+
+CLOUDINARY_API_SECRET = config(
+    "CLOUDINARY_API_SECRET",
+    default="",
+)
+
+import cloudinary
+
+cloudinary.config(
+    cloud_name=CLOUDINARY_CLOUD_NAME,
+    api_key=CLOUDINARY_API_KEY,
+    api_secret=CLOUDINARY_API_SECRET,
+)
