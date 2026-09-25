175ca8b39bcc1fc3d900532b3edcb5c8fb024672
Hexa Quebec <hexaquebec80@gmail.com>
2026-09-25 00:51:51 -0400

    Configurer PostgreSQL et Render

--- Belly/settings.py
+++ Belly/settings.py
@@ -278,3 +278,22 @@ DEFAULT_FROM_EMAIL = config(
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
